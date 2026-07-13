32077cfa4def9388b5281d8db20d48de861aea92
kitasael-burakku <keikoseoyeon@gmail.com>
2026-07-12 19:43:44 -0600

    dotfiles: backup 2026-07-12 19:43

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -16,14 +16,20 @@
 --                              \    Y    /\___  ||  |_> >  | \/  |__/ __ \|   |  \/ /_/ | 
 --                               \___|_  / / ____||   __/|__|  |____(____  /___|  /\____ | 
 --                                     \/  \/     |__|                   \/     \/      \/ 
-require("modules.animations")
-require("modules.autostart")
-require("modules.decoration")
+
 require("modules.environment")
+
+require("modules.monitors")
 require("modules.input")
+
+require("modules.animations")
+require("modules.decoration")
+
+require("modules.layout")
+require("modules.windowrules")
+
 require("modules.programs")
 require("modules.keybinds")
-require("modules.layout")
+
 require("modules.misc")
-require("modules.monitors")
-require("modules.windowrules")
+require("modules.autostart")
