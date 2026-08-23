7765fccfc6d628349bddff29cd83208e035e5d72
kitasael-burakku <280549934+kitasael-burakku@users.noreply.github.com>
2026-08-23 11:41:13 -0600

    Changed cursor theme

--- hypr/modules/environment.lua
+++ hypr/modules/environment.lua
@@ -8,7 +8,7 @@
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
 -- Variables
-CursorTheme = "Future-black-cursors"
+CursorTheme = "Nordic_Cursors_Scalable"
 GTKTheme = "Win11-Fantasy-Dark"
 
 -- Nombre bonito de la máquina. Vive acá y no en `hostnamectl --pretty`
